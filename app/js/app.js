a6b832c82c517eda8f9015eb4ce7de9b52195252
CharlieGreenman <charlie@Charlies-MacBook-Pro.local>
2015-12-26 22:50:03 -0500

    push redux code

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
 require("../scss/core.scss");
 
-var txt = require("./_anim.js") + "l";
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
+
+const store = createStore(counter);
+
+const render = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: "INCREMENT"
+        })
+      }
+      onDecrement={() =>
+        store.dispatch({
+          type: "DECREMENT"
+        })
+      }
+    />,
+    document.getElementById('drawing_container')
+  );
+};
+
+store.subscribe(render);
+render();
